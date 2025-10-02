9c16c1b28188193cddb1d187276d3915aa736988
zachmahan <zach.mahan.zm@gmail.com>
2025-10-02 15:10:20 -0400

    wip strimap

--- src/containers/strimap.c
+++ src/containers/strimap.c
@@ -1,28 +1,46 @@
 #include "strimap.h"
+#include <stdint.h>
 #include <stdlib.h>
 
-strimap_t strimap_create(size_t size) {
+strimap_t strimap_create(size_t starting_bucket_count) {
     strimap_t map;
-    map.size = size;
-    map.buckets = (strimap_entry_t**)calloc(size, sizeof(strimap_entry_t*)); // Initialize to NULL
+    map.bucket_count = starting_bucket_count;
+    map.buckets = (strimap_entry_t**)calloc(starting_bucket_count,
+                                            sizeof(strimap_entry_t*)); // Initialize to NULL
     return map;
 }
 void strimap_destroy(strimap_t* map) {
     if (!map || !map->buckets) {
         return;
     }
-    for (int i = 0; i < map->size; i++) {
+    for (int i = 0; i < map->bucket_count; i++) {
         strimap_entry_t* curr = map->buckets[i];
         while (curr) {
             strimap_entry_t* next = curr->next;
-            free(curr->key); // since key was malloc'd
+            free(curr->key); // since key string was malloc'd
             free(curr);
             curr = next;
         }
     }
     free((void*)map->buckets); // free buckets themselves
     map->buckets = NULL;
-    map->size = 0;
+    map->bucket_count = 0;
 }
+
+uint64_t hash_string(const char* str) {
+    unsigned long hash = 5381;
+    int curr_ch;
+
+    while ((curr_ch = (unsigned char)*str++)) { // iter
+        // hash = hash * 33 + c (DJB2 algorithm)
+        hash = ((hash << 5) + hash) + curr_ch;
+    }
+
+    return hash;
+}
+
+void strimap_insert(strimap_t* map, char* key, int val) {}
+
+void strimap_rehash(strimap_t* map, size_t new_bucket_count) {}
 
 // TODO finish impl
--- src/containers/strimap.h
+++ src/containers/strimap.h
@@ -2,6 +2,9 @@
 #define CONTAINERS_STRIMAP_H
 
 #include <stddef.h>
+#include <stdint.h>
+
+#define STRIMAP_LOAD_FACTOR .75
 
 typedef struct strimap_entry_t {
     char* key;
@@ -11,12 +14,16 @@ typedef struct strimap_entry_t {
 
 typedef struct {
     strimap_entry_t** buckets;
-    size_t size;
+    size_t bucket_count;
+    size_t entry_count;
 } strimap_t;
 
-strimap_t strimap_create(size_t size);
+strimap_t strimap_create(size_t starting_bucket_count);
 void strimap_destroy(strimap_t* map);
 void strimap_insert(strimap_t* map, char* key, int val);
+void strimap_rehash(strimap_t* map, size_t new_size);
 int strimap_at(strimap_t* map, char* key);
 
+// helper
+uint64_t hash_string(const char* str);
 #endif // ! CONTAINERS_STRIMAP_H
