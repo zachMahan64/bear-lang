baa31daa11059dfabe16540ad1ce7be93484ea54
zachmahan <zach.mahan.zm@gmail.com>
2025-10-03 13:14:21 -0400

    wip token maps

--- src/compiler/token.c
+++ src/compiler/token.c
@@ -1,2 +1,15 @@
 #include "token.h"
 #include "containers/strimap.h"
+#include <stdbool.h>
+
+int* get_char_to_token_map(void) {
+    static bool initialized = false;
+    static int char_to_token_map[128]; // for direct char indexing
+    if (!initialized) {
+        char_to_token_map['('] = LPAREN;
+        char_to_token_map[')'] = RPAREN;
+        // TODO finish
+        initialized = true;
+    }
+    return char_to_token_map;
+}
