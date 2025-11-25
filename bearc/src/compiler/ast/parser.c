42882e9786b03e5255e44517fae85ffd39dc2444
zachmahan <zach.mahan.zm@gmail.com>
2025-11-25 12:40:16 -0500

    update parser helper functions

--- bearc/src/compiler/ast/parser.c
+++ bearc/src/compiler/ast/parser.c
@@ -232,18 +232,21 @@ bool parser_eof(parser_t* parser) {
 
 // map containing look-ups for builtin types
 static const bool parser_builtin_type_map[TOK__NUM] = {
-    [TOK_CHAR] = true,      [TOK_U8] = true,  [TOK_I8] = true,   [TOK_I32] = true,
-    [TOK_U32] = true,       [TOK_I64] = true, [TOK_U64] = true,  [TOK_USIZE] = true,
-    [TOK_F32] = true,       [TOK_F64] = true, [TOK_BOOL] = true, [TOK_STR] = true,
-    [TOK_MODULE] = true,    [TOK_FN] = true,  [TOK_VAR] = true,  [TOK_VOID] = true,
-    [TOK_SELF_TYPE] = true, [TOK_BOX] = true, [TOK_BAG] = true,  [TOK_OPT] = true,
-    [TOK_RES] = true};
+    [TOK_CHAR] = true,     [TOK_U8] = true,  [TOK_I8] = true,   [TOK_I32] = true,
+    [TOK_U32] = true,      [TOK_I64] = true, [TOK_U64] = true,  [TOK_USIZE] = true,
+    [TOK_F32] = true,      [TOK_F64] = true, [TOK_BOOL] = true, [TOK_STR] = true,
+    [TOK_MODULE] = true,   [TOK_FN] = true,  [TOK_VAR] = true,  [TOK_VOID] = true,
+    [TOK_SELF_TYPE] = true};
+
+static const bool parser_builtin_type_mod_map[TOK__NUM] = {
+    [TOK_BOX] = true, [TOK_BAG] = true, [TOK_OPT] = true, [TOK_RES] = true};
 
 // match helpers
 bool parser_match_is_builtin_type(token_type_e t) { return parser_builtin_type_map[t]; }
 bool parser_match_is_builtin_type_or_id(token_type_e t) {
     return parser_builtin_type_map[t] || t == TOK_IDENTIFIER;
 }
+bool parser_match_is_builtin_type_mod(token_type_e t) { return parser_builtin_type_mod_map[t]; }
 
 // ^^^^^^^^^^^^^^^^ token consumption primitive functions ^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
