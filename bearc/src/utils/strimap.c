5344dcb306225fc90b6c2818813c8c81867b701c
zachMahan64 <zach.mahan.zm@gmail.com>
2025-12-09 16:36:33 -0500

    restructure utils header structure

--- bearc/src/utils/strimap.c
+++ bearc/src/utils/strimap.c
@@ -2,7 +2,7 @@
 // Copyright (C) 2025 Zachary Mahan
 // Licensed under the GNU GPL v3. See LICENSE.md for details.
 
-#include "strimap.h"
+#include "utils/strimap.h"
 #include "utils/arena.h"
 #include <stddef.h>
 #include <stdint.h>
