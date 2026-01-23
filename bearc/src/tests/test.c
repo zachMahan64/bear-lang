2cd1e4b8b7a1e04e8b4ef833fcfe9df212c5c948
zachMahan64 <zach.mahan.zm@gmail.com>
2026-01-22 20:59:48 -0500

    add another test case for variant parsing

--- bearc/src/tests/test.c
+++ bearc/src/tests/test.c
@@ -122,6 +122,7 @@ br_test_result_t test_parser(void) {
     ASSERT_EQ_ERR("43", 0);
     ASSERT_EQ_ERR("44", 1);
     ASSERT_EQ_ERR("45", 0);
+    ASSERT_EQ_ERR("46", 0);
 
     return TEST_RESULT;
 }
