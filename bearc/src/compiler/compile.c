04b74dc8ff38c18cf94b8bcd0a7f7d0e958558f0
zachMahan64 <zach.mahan.zm@gmail.com>
2026-01-26 05:30:13 -0500

    fix top level compile_file function error list ownership semantics; add a clarifying comment for placeholder logic

--- bearc/src/compiler/compile.c
+++ bearc/src/compiler/compile.c
@@ -22,27 +22,31 @@ int compile_file(const bearc_args_t* args) {
     const char* file_name = args->input_file_name;
     br_ast_t ast = ast_create_from_file(file_name);
 
+    // --token-table
     if (args->flags[CLI_FLAG_TOKEN_TABLE]) {
         print_out_src_buffer(&ast.src_buffer);
         print_out_tkn_table(&ast.tokens);
     }
+
+    // --pretty-print
     if (args->flags[CLI_FLAG_PRETTY_PRINT]) {
         pretty_print_stmt(ast.file_stmt_root_node);
     }
     // ----------------------------------------------------
 
+    // placeholder until we have a registry of multi-file compilation
     // display all comptime errors
     bool silent = args->flags[CLI_FLAG_SILENT];
-    compiler_error_list_t error_list = ast.error_list;
+    compiler_error_list_t* error_list = &ast.error_list;
     if (!silent) {
-        compiler_error_list_print_all(&error_list);
+        compiler_error_list_print_all(error_list);
     }
-    if (!compiler_error_list_empty(&error_list)) {
+    if (!compiler_error_list_empty(error_list)) {
         if (!silent) {
             printf("compilation terminated: %s'%s'\n%s", ansi_bold_white(), file_name,
                    ansi_reset());
         }
-        code = (int)compiler_error_list_count(&error_list);
+        code = (int)compiler_error_list_count(error_list);
     }
 
     // clean up resources
